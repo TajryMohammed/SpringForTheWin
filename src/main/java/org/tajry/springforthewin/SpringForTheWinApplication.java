d931e14386f6a4575377a5b7dc332246d5dceebb
Tajry Mohammed <tajry.mohammed@gmail.com>
2025-11-16 17:51:59 +0100

    chore: initial project setup

---
+++ src/main/java/org/tajry/springforthewin/SpringForTheWinApplication.java
@@ -0,0 +1,13 @@
+package org.tajry.springforthewin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForTheWinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringForTheWinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tajry/springforthewin/SpringForTheWinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tajry.springforthewin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForTheWinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
